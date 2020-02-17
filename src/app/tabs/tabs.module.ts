826eb10318ac10cfd4f183a57301a1c0bcbb99dc
othnielalexander <othniel0607@gmail.com>
2020-02-17 13:15:15 +0530

    new_monday_new

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -2,6 +2,7 @@ import { IonicModule } from '@ionic/angular';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+// import { ExpandableComponent } from "../components/expandable/expandable.component";
 
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
